8e1f325b43487caefaa551d781afac5a4fccf561
郑金光 <zhengjinguangzjg@163.com>
2020-09-14 16:24:05 +0800

    换算系数常量

--- src/main/java/com/xsdzq/mall/constants/CreditRecordConst.java
+++ src/main/java/com/xsdzq/mall/constants/CreditRecordConst.java
@@ -5,7 +5,10 @@ public interface CreditRecordConst {
 	// 增减积分
 	boolean ADDSCORE = true;
 	boolean REDUCESCORE = false;
-	
+
+	// 换算系数
+	int EXCHANGECOEFFICIENT = 100;
+
 	// item 系列
 	String NEWACCOUNT = "10";
 	String NEWACCOUNTITEM = "新开户";
