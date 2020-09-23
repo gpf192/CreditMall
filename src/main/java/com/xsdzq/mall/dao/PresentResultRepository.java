c5d274896cc6d86f74205edfc6a8953e078c0ce9
郑金光 <zhengjinguangzjg@163.com>
2020-09-23 12:16:04 +0800

    查询最近抽奖记录

--- src/main/java/com/xsdzq/mall/dao/PresentResultRepository.java
+++ src/main/java/com/xsdzq/mall/dao/PresentResultRepository.java
@@ -2,6 +2,8 @@ package com.xsdzq.mall.dao;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.xsdzq.mall.entity.MallUserEntity;
@@ -9,9 +11,13 @@ import com.xsdzq.mall.entity.PresentResultEntity;
 
 public interface PresentResultRepository extends JpaRepository<PresentResultEntity, Long>, PresentResultSweeper {
 
+	Page<PresentResultEntity> findByOrderByRecordTimeDesc(Pageable pageable);
+	
 	List<PresentResultEntity> findByMallUserEntityOrderByRecordTimeDesc(MallUserEntity mallUserEntity);
 
 	List<PresentResultEntity> findByMallUserEntityAndDateFlagOrderByRecordTimeDesc(MallUserEntity mallUserEntity,
 			String dateFlag);
+	
+	
 
 }
