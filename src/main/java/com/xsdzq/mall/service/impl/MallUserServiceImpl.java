b1a7a424a1449f47eddeced8dcf11f09a037f3c1
郑金光 <zhengjinguangzjg@163.com>
2021-01-25 12:11:10 +0800

    增加nickname校验

--- src/main/java/com/xsdzq/mall/service/impl/MallUserServiceImpl.java
+++ src/main/java/com/xsdzq/mall/service/impl/MallUserServiceImpl.java
@@ -82,7 +82,7 @@ public class MallUserServiceImpl implements MallUserService {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	public boolean hsServiceCheck(String clientId, String loginClientId, String accessToken) {
+	public boolean hsServiceCheck(String clientId, String loginClientId, String accessToken, String mobile) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("access_token", accessToken);
 		HttpEntity<String> requestEntity = new HttpEntity<>(null, headers);
@@ -105,7 +105,9 @@ public class MallUserServiceImpl implements MallUserService {
 
 				JSONObject hsJsonObject = JSON.parseObject(response);
 				String responseClientId = hsJsonObject.getString("client_id");
-				if (responseClientId != null && loginClientId.equals(responseClientId)) {
+				String nickName = hsJsonObject.getString("nick_name");
+				if (responseClientId != null && loginClientId.equals(responseClientId) && nickName != null
+						&& nickName.equals(mobile)) {
 					// 校验通过
 					log.info(loginClientId + " 校验通过");
 					return true;
@@ -142,7 +144,8 @@ public class MallUserServiceImpl implements MallUserService {
 		// TODO Auto-generated method stub
 		// 0 前置 恒生校验 第一个版本需要注释
 		if (user.getLoginClientId() != null && user.getLoginClientId().length() > 0) {
-			boolean isCheck = hsServiceCheck(user.getClientId(), user.getLoginClientId(), user.getAccessToken());
+			boolean isCheck = hsServiceCheck(user.getClientId(), user.getLoginClientId(), user.getAccessToken(),
+					user.getMobile());
 			if (!isCheck) {
 				return null;
 			}
