975b5b3d975da8931ba4caa0d65846379b5583bf
郑金光 <zhengjinguangzjg@163.com>
2021-05-12 15:29:40 +0800

    处理过期时间

--- src/main/java/com/xsdzq/mall/util/DateUtil.java
+++ src/main/java/com/xsdzq/mall/util/DateUtil.java
@@ -5,6 +5,10 @@ import java.util.Date;
 
 public class DateUtil {
 
+	public static void main(String[] args) {
+		getReduceDate("20200101");
+	}
+
 	public static String getStandardDate(Date date) {
 		SimpleDateFormat sFormat = new SimpleDateFormat("yyyy-MM-dd");
 		String standardString = sFormat.format(date);
@@ -21,8 +25,27 @@ public class DateUtil {
 		SimpleDateFormat sFormat = new SimpleDateFormat("yyyyMMdd");
 		String fullString = sFormat.format(date);
 		int fullTime = Integer.parseInt(fullString);
-		System.out.println(fullTime);
+		// System.out.println(fullTime);
 		return fullTime;
+	}
+
+	public static int getReduceDate(String source) {
+
+		int year = Integer.parseInt(source.substring(0, 4));
+		int month = Integer.parseInt(source.substring(4, 6));
+		int day = Integer.parseInt(source.substring(6, 8));
+
+		Date date = new Date(year - 1900, month - 1, day);
+		// System.out.println(getIntegerTime(date));
+		// System.out.println(date.getTime());
+
+		long mi = date.getTime() - 24 * 1000 * 60 * 60;
+		Date reduceDate = new Date(mi);
+		// System.out.println(getIntegerTime(reduceDate));
+		// int = reduceDate.get
+
+		return getIntegerTime(reduceDate);
+
 	}
 
 }
